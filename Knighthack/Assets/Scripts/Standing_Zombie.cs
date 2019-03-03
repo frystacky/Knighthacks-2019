b6f4e869a79bc57558fb6aae46f86332fb4da760
Adam Wood <awood0511@ufl.edu>
2019-03-02 23:10:23 -0500

    zombie unsticks himself, also prefab modifications

--- Knighthack/Assets/Scripts/Standing_Zombie.cs
+++ Knighthack/Assets/Scripts/Standing_Zombie.cs
@@ -10,8 +10,9 @@ public class Standing_Zombie : MonoBehaviour
     Vector3 xyz;
     Vector3 knight_xyz;
     [SerializeField] Sprite[] directionFacing;
-    bool chasing = false;
     private SpriteRenderer facing;
+    bool chasing = false;
+    int stuck_frames = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,27 +20,38 @@ public class Standing_Zombie : MonoBehaviour
         player = GameObject.FindGameObjectsWithTag("Player")[0];
         rb2d = GetComponent<Rigidbody2D>();
         facing = gameObject.GetComponent<SpriteRenderer>();
+        xyz = transform.position;
+        knight_xyz = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        //get zombie unstuck if chasing and not moving in x
+
+        //get zombie unstuck if chasing and not moving in xd
         if (chasing && xyz.x == transform.position.x)
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 20);
+            stuck_frames++;
+            if (stuck_frames > 20)
+            {
+                stuck_frames = 0;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 20);
+            }
         }
-        */
+        else
+        {
+            stuck_frames = 0;
+        }
 
         xyz = transform.position;
         knight_xyz = player.transform.position;
+
         //check distance
-        if (Vector3.Distance(xyz, knight_xyz) < 25.0)
+        if (Vector3.Distance(xyz, knight_xyz) < 25.0 || (chasing && Vector3.Distance(xyz, knight_xyz) <= 35))
         {
             chasing = true;
             //zombie chases left
-            if(xyz.x < knight_xyz.x)
+            if (xyz.x < knight_xyz.x)
             {
                 facing.sprite = directionFacing[1];
                 rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
@@ -50,25 +62,27 @@ public class Standing_Zombie : MonoBehaviour
                 rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
             }
         }
-        else if (Vector3.Distance(xyz, knight_xyz) > 35.0)
+        else if (Vector3.Distance(xyz, knight_xyz) > 35)
         {
             chasing = false;
+            facing.sprite = directionFacing[2];
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
+
     }
 
     //zombie deals damage and knocks back the knight
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject collide = collision.gameObject;
-        if(collide.tag == "Player")
+        if (collide.tag == "Player")
         {
             Rigidbody2D knight_rb2d = collide.GetComponent<Rigidbody2D>();
 
             //knockback right
             if (xyz.x < knight_xyz.x)
             {
-                knight_rb2d.velocity = new Vector2(50,75);
+                knight_rb2d.velocity = new Vector2(50, 75);
             }
             else
             {
